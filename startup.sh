8655ab9c0cdad8ae18eaa5fe1cc31ccdc2443564
aivaras-sukackas <61233136+aivaras-sukackas@users.noreply.github.com>
2020-10-05 21:21:31 +0300

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/aivaras-sukackas/eks.git
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+terraform -install-autocomplete
