fa4353f50c60c494d464164bd6979aea51819248
aivaras-sukackas <61233136+aivaras-sukackas@users.noreply.github.com>
2020-10-05 23:19:37 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install terraform jq
 terraform -install-autocomplete
+
+aws iam create-service-linked-role --aws-service-name ecs.amazonaws.com
+
+rm -vf ${HOME}/.aws/credentials
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+
