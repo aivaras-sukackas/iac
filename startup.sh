ba5effbf7bc6fbd9b33522228a97158c30fb4ae1
aivaras-sukackas <61233136+aivaras-sukackas@users.noreply.github.com>
2020-10-05 21:23:22 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git clone https://github.com/aivaras-sukackas/eks.git
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
